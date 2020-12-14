e2be2fe91f2f9718358d0e748158085d8ce910cb
dendi.db <budidendi1234@gmail.com>
2020-12-15 00:13:45 +0800

    Update Medi dan CSS BizMetch

---
+++ practice/css-bizmetch/assets/js/index.js
@@ -0,0 +1,16 @@
+var checkCount = 0
+var maxChecks = 5
+function setChecks(obj) {
+  console.log(checkCount)
+  if (obj.checked) {
+    checkCount = checkCount + 1
+  }else{
+    checkCount = checkCount -1
+  }
+  if (checkCount > maxChecks){
+    obj.checked = false
+    checkCount = checkCount -1
+  }
+}
+
+
