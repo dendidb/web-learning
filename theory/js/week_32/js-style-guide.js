d1645ea23becced42e6bc448c8bd71b17f343dc5
dendi.db <budidendi1234@gmail.com>
2021-03-20 16:33:03 +0700

    add materi

--- theory/js/week_32/js-style-guide.js
+++ theory/js/week_32/js-style-guide.js
@@ -9,4 +9,25 @@ JavaScript Style Guide
   Tambahkan spasi setelah koma pada array
 [3] Code Indentation
   Gunakan Indentation 2 spasi
-  
+[4] Aturan Statement
+- selalu tutup dengan titik koma (;) pada akhir syntax (Statement JS)
+  [] complex statement (function, loop, conditionals)
+  - Tambahkan kurung kurawal buka di akhir baris
+
+
+//  LANJUT DI RUMAH =================
+
+
+
+
+  **
+  kebab-case : biasanya digunakan pada nama class
+  snake_case : biasanya pada SQL Database dan bahasa PHP
+  PascalCase : biasanya digunakan bahasa C, Java, nama class JavaScript
+  camelCase : biasanya untuk bahasa JavaScript untuk namaVariable, function
+[7] Tempatkan JS di bawah sebelum close tag body
+[8] File Extensions
+- JS itu pake .js
+[9] Nama File
+- GUnakan kebab-case/PascalCase/camelCase untuk nma file JS
+
