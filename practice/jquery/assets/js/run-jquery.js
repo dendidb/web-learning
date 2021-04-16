b22ec5d18d65a2945fdf3ba816b45dc617fe9b84
dendi.db <budidendi1234@gmail.com>
2021-04-16 08:04:44 +0700

    practice jQuery

--- practice/jquery/assets/js/run-jquery.js
+++ practice/jquery/assets/js/run-jquery.js
@@ -1,4 +1,4 @@
-// show hide
+// show and hide
 $('#hideButton').on('click', function(){
   if($('#hideButton').text() === 'Hide') {
     $('img').hide(600);
@@ -11,22 +11,80 @@ $('#hideButton').on('click', function(){
 
 
 // carousel basic
-
-// $('.js-effect').on('click', function(){
-//   $('boxxx').animate({
-//     width: '640px'
-//   }, 'slow');
-// });
-
-
 var _post = 0;
-$('.js-slide-nav').on('click', function() {
-  if ($(this).hasClass('slide-c__btn--next')) {
-    _next += 240;
+$('.js-carousel-nav').on('click', function(e){
+  if ($(this).hasClass('carousel__btn--next')) {
+    _post += 240;
   } else {
     _post -= 240;
   }
-  $('.slide-c__box').animate({
-    left: '-' + _post + 'px'
-  }, 'slow');
+  $('.carousel__box').animate({
+    left: '-' + _post + 'px'},
+    '600');
 });
+
+
+// function to do list
+const WHITESPACE = /^ *$/;
+
+$('.js-input-menu').on('keypress', function(e) {
+  if (e.keyCode === 13) {
+    var _val = $(this).val(),
+    _item = '<div class="alert alert-success">'+
+              '<p class="item">' +_val +'</p>'+
+              '<button type="button" class="delete">' + 'X' +'</button>'+ '</div>';
+    $('.js-list').append(_item);
+    $(this).val('');
+  }
+});
+
+// $('body').on('click','.delete', function(e) {
+//   $(this).parent().remove();
+// });
+
+// var dropdown = {
+//   handleClick function() {
+//     $('.js-dropdown .btn').on('click', function(e){
+//       if ($(this).parents('dropdown'))
+//     });
+//   }
+// }
+
+
+// functon slide down
+// $('#clickMe').click(function () {
+//   if ($('.kuy').first().is(':hidden')) {
+//     $('.kuy').slideDown('slow');
+//     $('#clickMe').click(function(){
+//       if
+//     });
+//   } else {
+//     $('.kuy').hide();
+//   }
+// });
+
+
+
+// ============= function popup-team ========= //
+
+
+
+
+// ============= function popup-team ========= //
+
+// function video
+
+// var video = document.getElementById('myVideo'),
+// btn = document.getElementById('btnJs');
+
+// btnJs.onclick(function btnClicked() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = 'Pause';
+//   } else {
+//     video.paused();
+//     btn.innerHTML = 'Play';
+//   }
+// })
+
+
---
+++ theory/jQuery/practice/week_2/jquery-html.js
@@ -0,0 +1,51 @@
+// Getter
+
+JQUERY HTML
+--
+
+Text
+--
+selector.text();
+
+HTML Tag
+--
+selector.html();
+
+Value Input
+--
+
+selector.val()
+
+Value Atribute
+--
+
+selector.attr('attribute-name');
+
+
+
+// setter
+SET
+--
+
+Text
+--
+selector.text();
+
+HTML Tag
+--
+selector.html();
+
+Value Input
+--
+
+selector.val()
+
+Value Atribute
+--
+
+selector.attr('attribute-name');
+
+
+
+$('.js-show-hide'),on('click');
+bui
